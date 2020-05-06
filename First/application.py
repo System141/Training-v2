05d17c16b7b0f927e90cc25a52ebfb088d56493c
System141 <thesystem141@gmail.com>
2020-05-06 23:15:21 +0300

    Update

---
+++ First/application.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, world!"
+
+@app.route("/ali")
+def ali():
+    return "Hello, David!"
---
+++ variables0/application.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    headline = "Hello!"
+    return render_template("index.html", headline=headline)
+
+@app.route("/bye")
+def bye():
+    headline = "Goodbye!"
+    return render_template("index.html", headline=headline)
+
+@app.route("/potato")
+def potato():
+    return "Hello, world!"
